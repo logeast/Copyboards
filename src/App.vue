de4327064ff152709344372d62c03d76bb8d741b
logeast <logeast@outlook.com>
2022-07-02 07:05:49 +0800

    feat: format project

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,20 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import CopyItem from './components/CopyItem.vue'
+
+const copylist = ref([
+    { id: 0, text: '为众人抱薪者，不可使其冻毙于风雪。' },
+    { id: 1, text: '为大众谋福利者，不可使其孤军奋战。' },
+    { id: 2, text: '为自由开路者，不可使其困顿于荆棘。' },
+    { id: 3, text: '遍身罗绮者，不是养蚕人。' },
+    { id: 4, text: '朱门酒肉臭，路有冻死骨。' }
+])
 </script>
 
 <template>
-  <div class="logo-box">
-    <img style="height:140px;" src="./assets/electron.png" >
-    <span/>
-    <img style="height:140px;" src="./assets/vite.svg" >
-    <span/>
-    <img style="height:140px;" src="./assets/vue.png" >
-  </div>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <div class="static-public">
-    Place static files into the <code>/public</code> folder
-    <img style="width:77px;" :src="'./node.png'" >
-  </div>
+    <ul>
+        <CopyItem v-for="item in copylist" :value="item" :key="item.id"></CopyItem>
+    </ul>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.logo-box {
-  display: flex;
-  width: 100%;
-  justify-content: center;
-}
-.logo-box span {
-  width: 74px;
-}
-.static-public {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.static-public code {
-  background-color: #eee;
-  padding: 2px 4px;
-  margin: 0 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
---
+++ src/components/CopyItem.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps({
+    value: Object,
+})
+</script>
+
+<template>
+    <li class="px-2 py-2 border-b">{{ value.text }}</li>
+</template>
