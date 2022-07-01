41ba3a2173ef138f400787de0f9797400f590add
logeast <logeast@outlook.com>
2022-07-01 17:31:58 +0800

    feat add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+// import './samples/node-api'
+import "./index.css";
 
-createApp(App).mount('#app')
+createApp(App)
+  .mount("#app")
+  .$nextTick(() => {
+    postMessage({ payload: "removeLoading" }, "*");
+  });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { rmSync } from 'fs'
+import { join } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import electron from 'vite-plugin-electron'
+import pkg from './package.json'
 
-const path = require("path");
+rmSync('dist', { recursive: true, force: true }) // v14.14.0
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, "./dist"),
-  plugins: [vue()],
-});
+  plugins: [
+    vue(),
+    electron({
+      main: {
+        entry: 'electron/main/index.ts',
+        vite: {
+          build: {
+            sourcemap: false,
+            outDir: 'dist/electron/main',
+          },
+        },
+      },
+      preload: {
+        input: {
+          // You can configure multiple preload here
+          index: join(__dirname, 'electron/preload/index.ts'),
+        },
+        vite: {
+          build: {
+            // For debug
+            sourcemap: 'inline',
+            outDir: 'dist/electron/preload',
+          }
+        }
+      },
+      // Enables use of Node.js API in the Renderer-process
+      renderer: {},
+    }),
+  ],
+  server: {
+    host: pkg.env.VITE_DEV_SERVER_HOST,
+    port: pkg.env.VITE_DEV_SERVER_PORT,
+  },
+})
