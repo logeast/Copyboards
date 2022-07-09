b5281a5c2b3f346456971649cae37f0cea2f5d10
logeast <logeast@outlook.com>
2022-07-09 10:48:49 +0800

    feat: unhandle commit

--- src/renderer/components/Copyboards.vue
+++ src/renderer/components/Copyboards.vue
@@ -1,7 +1,99 @@
 <script setup lang="ts">
 import { useClipboard } from '../composables';
 import { ref, computed, watchEffect } from 'vue';
-const { readText } = useClipboard();
+import { nativeImage } from 'electron';
+const { readText, readImage } = useClipboard();
+
+interface Options {
+  duration?: number;
+  textChange?: (text: string, beforeText: string) => void;
+  imageChange?: (image: nativeImage, beforeImage: nativeImage) => void
+}
+
+class ClipboardObserver {
+  timer: any;
+  beforeText: string;
+  beforeImage: string;
+
+  duration = 500;
+  textChange: (text: string, beforeText: string) => void;
+  imageChange: (image: nativeImage, beforeImage: nativeImage) => void;
+
+  constructor(options: Options) {
+    const { duration, textChange, imageChange } = options;
+
+    this.duration = duration;
+    this.textChange = textChange;
+    this.imageChange = imageChange;
+
+    if (this.textChange || this.imageChange) {
+      this.start();
+    }
+  }
+
+  start(): void {
+
+  }
+
+  stop(): void {
+    this.setTimer();
+  }
+
+  clearTimer(): void {
+    clearInterval(this.timer);
+  }
+
+  setClipboardDefaultValue(): void {
+    if (this.textChange) {
+      this.beforeText = readText();
+    }
+    if (this.imageChange) {
+      this.beforeImage = readImage();
+    }
+  }
+
+  setTimer(): void {
+    const callback = () => {
+      if (this.textChange) {
+        const text = readText();
+        if (this.isDiffText(this.beforeText, text)) {
+          this.textChange(text, this.beforeText);
+          this.beforeText = text;
+        }
+      }
+
+      if (this.imageChange) {
+        const image = readImage();
+
+        if (this.isDiffImage(this.beforeImage, image)) {
+          this.imageChange(image, this.beforeImage);
+          this.beforeImage = image;
+        }
+      }
+    };
+
+    this.timer = setInterval(callback, this.duration);
+  }
+
+  isDiffText(beforeText: string, afterText: string): boolean {
+    return Boolean(afterText) && beforeText !== afterText;
+  }
+
+  isDiffImage(beforeImage: nativeImage, afterImage: nativeImage): boolean {
+    return Boolean(afterImage) && !afterImage.isEmpty() && beforeImage.toDataURL() !== afterImage.toDataURL();
+  }
+}
+
+const clipboardObserver = new ClipboardObserver({
+  textChange: (text: string, beforeText: string) => {
+    console.log('text', text);
+  },
+  imageChange: (image: nativeImage, beforeImage: nativeImage) => {
+    console.log('image', image);
+  }
+});
+
+clipboardObserver.start();
 
 interface ClipType {
   id: number;
