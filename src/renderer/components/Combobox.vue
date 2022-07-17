5749d831ebf28e611aaf5ca31f2724885585287f
logeast <logeast@outlook.com>
2022-07-17 23:09:48 +0800

    feat: update combobox

--- src/renderer/components/Combobox.vue
+++ src/renderer/components/Combobox.vue
@@ -1,79 +1,73 @@
-
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import {
   Combobox,
   ComboboxInput,
-  ComboboxButton,
   ComboboxOptions,
-  ComboboxOption,
-  TransitionRoot,
+  ComboboxOption
 } from '@headlessui/vue';
-import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid';
+import { SearchIcon } from '@heroicons/vue/solid';
 
-const people = [
-  { id: 1, name: 'Wade Cooper' },
-  { id: 2, name: 'Arlene Mccoy' },
-  { id: 3, name: 'Devon Webb' },
-  { id: 4, name: 'Tom Cook' },
-  { id: 5, name: 'Tanya Fox' },
-  { id: 6, name: 'Hellen Schmidt' },
+let uuid = 1000;
+const clips = [
+  { id: uuid++, text: '🎉 Congratulate!', date: new Date(), active: true },
+  { id: uuid++, text: 'Copyboards - A Better Way to Copy and Paste!', date: new Date(), active: true },
+  { id: uuid++, text: 'Copyboards - A Better Way to Copy and Paste! 🎉 Congratulate!', date: new Date(), active: true },
+  { id: uuid++, text: 'Image: custom-search.png(1280x768, 1.25M)', date: new Date(), active: true },
+  { id: uuid++, text: 'File: ~/document/copyboards/README.md(1.25M)', date: new Date(), active: true },
+  { id: uuid++, text: '#C084FC', date: new Date(), active: true },
+  { id: uuid++, text: 'This section describes major releases and their improvements. For a detailed list of changes …', date: new Date(), active: true }
 ];
 
-let selected = ref(people[0]);
-let query = ref('');
+const selected = ref(clips[0]);
+const query = ref('');
 
-let filteredPeople = computed(() =>
+const filteredClips = computed(() =>
   query.value === ''
-    ? people
-    : people.filter((person) =>
-      person.name
-        .toLowerCase()
-        .replace(/\s+/g, '')
-        .includes(query.value.toLowerCase().replace(/\s+/g, ''))
+    ? clips
+    : clips.filter(({ id, text, date }) =>
+      [id, text, date].some((val) =>
+        val
+          .toString()
+          .toLocaleLowerCase()
+          .includes(query.value.toLocaleLowerCase().replace(/\s+/g, ''))
+      )
     )
 );
+
 </script>
 
 <template>
-  <div class="fixed top-16 w-72">
-    <Combobox v-model="selected">
-      <div class="relative mt-1">
-        <div
-          class="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-          <ComboboxInput class="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-            :displayValue="(person) => person.name" @change="query = $event.target.value"></ComboboxInput>
-          <ComboboxButton class="absolute inset-y-0 right-0 flex items-center pr-2">
-            <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true"></SelectorIcon>
-          </ComboboxButton>
-        </div>
-        <!-- <TransitionRoot leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0"
-          @after-leave="query = ''"> -->
-        <ComboboxOptions static
-          class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-          <div v-if="filteredPeople.length === 0 && query !== ''"
-            class="relative cursor-default select-none py-2 px-4 text-gray-700">
-            Nothing found.
-          </div>
+  <Combobox v-model="selected">
 
-          <ComboboxOption v-for="person in filteredPeople" as="template" :key="person.id" :value="person"
-            v-slot="{ selected, active }">
-            <li class="relative cursor-default select-none py-2 pl-10 pr-4" :class="{
-              'bg-teal-600 text-white': active,
-              'text-gray-900': !active,
-            }">
-              <span class="block truncate" :class="{ 'font-medium': selected, 'font-normal': !selected }">
-                {{ person.name }}
-              </span>
-              <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3"
-                :class="{ 'text-white': active, 'text-teal-600': !active }">
-                <CheckIcon class="h-5 w-5" aria-hidden="true"></CheckIcon>
-              </span>
-            </li>
-          </ComboboxOption>
-        </ComboboxOptions>
-        <!-- </TransitionRoot> -->
-      </div>
-    </Combobox>
-  </div>
+    <section class="flex items-center justify-between border-b px-3 gap-2 rounded-t-2xl bg-gray-50/75 backdrop-blur-2xl"
+      style="height: 56px">
+      <SearchIcon class="h-6 w-6"></SearchIcon>
+      <ComboboxInput
+        class="flex-1 h-full outline-none bg-transparent text-lg placeholder:text-gray-500 placeholder:font-normal"
+        :displayValue="(clip) => clip.text" @change="query = $event.target.value" placeholder="Copyboards Search">
+      </ComboboxInput>
+    </section>
+
+    <ComboboxOptions static class="px-3 py-2">
+      <section v-if="filteredClips.length === 0 && query !== ''"
+        class="relative cursor-default select-none py-2 px-4 text-gray-700">
+        Nothing found.
+      </section>
+
+      <ComboboxOption v-for="clip in filteredClips" as="template" :key="clip.id" :value="clip.text"
+        v-slot="{ selected, active }">
+        <li class="flex items-center justify-between px-2 gap-2 h-9 rounded-lg cursor-pointer" :class="{
+          'bg-blue-500 text-white': selected || active,
+        }">
+          <div class="flex items-center justify-center w-4 h-4 flex-none"></div>
+
+          <p class="flex-1 truncate" :title="clip.text">
+            {{ clip.text }}
+          </p>
+        </li>
+      </ComboboxOption>
+    </ComboboxOptions>
+
+  </Combobox>
 </template>
