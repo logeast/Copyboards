33afc68cd99d339b37e802771e1ce52e1d19b173
logeast <logeast@outlook.com>
2022-09-28 12:21:58 +0800

    feat: perfect the props

--- src/renderer/components/@headless/list/list.ts
+++ src/renderer/components/@headless/list/list.ts
@@ -5,15 +5,26 @@ import { defineComponent, h } from "vue";
  */
 export const List = defineComponent({
   name: "List",
+  emits: { "update:modelValue": (_value: any) => true },
   props: {
+    /**
+     * The element or component the `List` should render as.
+     */
     as: { type: [Object, String], default: "template" },
+    /**
+     * The selected value, the `List` supports `update:modelValue` emit.
+     * Usually use `v-model` to get and set the value.
+     */
     modelValue: { type: [Object, String, Number, Boolean], default: undefined },
+    /**
+     * The default value when using as an uncontrolled component.
+     */
     defaultValue: {
       type: [Object, String, Number, Boolean],
       default: undefined,
     },
   },
-  setup(props, { slots, expose }) {
+  setup(props, { slots }) {
     return () => {
       const { as, ...incomingProps } = props;
 
