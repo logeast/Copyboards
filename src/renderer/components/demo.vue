5082afbc5afb4385c49781aeebf4ac878e2dc636
logeast <logeast@outlook.com>
2022-10-13 17:16:07 +0800

    feat: add useClipboardLitener hook for listen to clipboard change

--- src/renderer/components/demo.vue
+++ src/renderer/components/demo.vue
@@ -11,6 +11,7 @@ const people = [
 ];
 
 const selectedPerson = ref(people[0]);
+
 </script>
 
 <template>
---
+++ src/renderer/hooks/@electron/use-clipboard-listener.ts
@@ -0,0 +1,84 @@
+import type { NativeImage } from "electron";
+import { useClipboard } from "./use-clipboard";
+import mitt from "mitt";
+
+const emitter = mitt();
+
+const clipboard = useClipboard();
+let listenerId: NodeJS.Timeout | null = null;
+let previousText = clipboard.readText();
+let previousImage = clipboard.readImage();
+
+export type ClipboardEventMap = {
+  text: string;
+  image: string;
+};
+
+export type Listener<T = unknown> = (event: T) => void;
+
+export interface useClipboardListenerReturn {
+  on(type: keyof ClipboardEventMap, listener: Listener): void;
+  off(type: keyof ClipboardEventMap, listener: Listener): void;
+  startListening(): any;
+  stopListening(): any;
+}
+
+/**
+ * Extended electron clipboard with more event listener.
+ */
+export function useClipboardListener(): useClipboardListenerReturn {
+  function on(type: keyof ClipboardEventMap, listener: Listener) {
+    emitter.on(type, listener);
+    return clipboard;
+  }
+
+  function off(type: keyof ClipboardEventMap, listener: Listener) {
+    emitter.off(type, listener);
+    return clipboard;
+  }
+
+  function startListening(ms: number = 500) {
+    if (listenerId) {
+      listenerId = setInterval(() => {
+        if (compareText(previousText, (previousText = clipboard.readText()))) {
+          emitter.emit("text" as keyof ClipboardEventMap);
+        }
+
+        if (
+          compareImage(previousImage, (previousImage = clipboard.readImage()))
+        ) {
+          emitter.emit("image" as keyof ClipboardEventMap);
+        }
+      }, ms);
+    }
+  }
+
+  function stopListening() {
+    if (listenerId) {
+      clearInterval(listenerId);
+    }
+    listenerId = null;
+    return clipboard;
+  }
+
+  return {
+    on,
+    off,
+    startListening,
+    stopListening,
+  };
+}
+
+/**
+ * Compare two text's values for equality.
+ */
+export function compareText(a: string, z: string): boolean {
+  return a !== z;
+}
+
+/**
+ * Compare two native image's values for equality.
+ */
+export function compareImage(a: NativeImage, z: NativeImage) {
+  return z && !z.isEmpty() && a.toDataURL() !== z.toDataURL();
+}
