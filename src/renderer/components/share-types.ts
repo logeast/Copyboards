850e44ebce11224e8cc78f12554e4ff1848d8cd2
logeast <logeast@outlook.com>
2022-10-17 12:16:58 +0800

    feat: define shared types

--- src/renderer/components/share-types.ts
+++ src/renderer/components/share-types.ts
@@ -1,8 +1,33 @@
-/** Listbox item props for copied item. */
+/** Listbox item text info props for copied message. */
+export interface ItemTextInfoProps {
+  metadata?: string;
+  /** Wheather the copied message is a color text. */
+  color?: boolean;
+}
+
+/** Listbox item image info props for copied message. */
+export interface ItemImageInfoProps {
+  /** data:image/png:base64 */
+  metadata?: string;
+}
+
+/** Listbox item props for copied message. */
 export interface ListboxItemProps {
-  id?: string | number;
+  /** The uniqued identify for copied message. */
+  id: string | number;
+
+  /** The icon of the app where the copied message comes from. */
   icon?: HTMLElement;
-  text?: string;
+
+  /** Listbox item text info props for copied message. */
+  textInfo?: ItemTextInfoProps;
+
+  /** Listbox item image info props for copied message. */
+  imageInfo?: ItemImageInfoProps;
+
+  /** Whether the copied message is active? */
   active?: boolean;
-  date?: Date;
+
+  /** The copide message created datetime. */
+  datetime?: Date;
 }
