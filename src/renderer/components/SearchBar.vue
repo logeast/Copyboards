3208595721f9fa3929c5c2a3d4073012687d184e
logeast <logeast@outlook.com>
2022-10-18 19:21:10 +0800

    feat: update icon

--- src/renderer/components/SearchBar.vue
+++ src/renderer/components/SearchBar.vue
@@ -12,23 +12,25 @@ withDefaults(defineProps<SearchBarProps>(), {
 
 <template>
   <section
-    class="flex items-center justify-between border-b px-3 gap-2 rounded-t-2xl bg-gray-100/90 backdrop-blur-xl text-gray-500"
+    class="flex items-center justify-between border-b px-3 gap-2 rounded-t-2xl bg-gray-100/90 backdrop-blur-xl"
     style="height: 56px"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      stroke-width="2"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-      ></path>
-    </svg>
+    <span class="text-gray-600">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        ></path>
+      </svg>
+    </span>
     <input
       class="flex-1 h-full outline-none bg-transparent text-lg placeholder:text-gray-400 placeholder:font-normal"
       autofocus
