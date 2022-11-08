9aaa57131562f5d48b190710a6c7533a2c3276de
Logeast <logeast@outlook.com>
2022-11-08 15:58:01 +0800

    fix: fix tray click error

--- src/main/index.ts
+++ src/main/index.ts
@@ -11,14 +11,14 @@ let mainWindow: AppWindow | null = null;
  * Global variables are created to avoid consrants bee collected when the next
  * collection cycle of GC(Garbage Collection) comes.
  *
- * @see https://www.electronjs.org/docs/latest/faq#my-apps-tray-disappeared-after-a-few-minutes
+ * @see
+ * https://www.electronjs.org/docs/latest/faq#my-apps-tray-disappeared-after-a-few-minutes
  */
 let tray: Tray | null = null;
 
 function createTray() {
-  const icon = nativeImage.createFromPath(
-    path.join(projectRoot, "assets", "trayicon.png")
-  );
+  const iconPath = path.join(projectRoot, "assets", "trayicon.png");
+  const icon = nativeImage.createFromPath(iconPath);
   /**
    * Adapt tray icon color when macOS theme or wallpaper changed.
    *
@@ -28,23 +28,12 @@ function createTray() {
 
   tray = new Tray(icon);
 
-  const contextMenu = Menu.buildFromTemplate([
-    { label: "item1", type: "radio" },
-    { label: "item2", type: "radio" },
-    { label: "item3", type: "radio" },
-    { label: "item4", type: "radio" },
-  ]);
-
   tray.on("click", () => {
     if (mainWindow?.isVisible()) {
       return mainWindow?.hide();
     } else {
       return mainWindow?.show();
     }
-  });
-
-  tray.on("right-click", () => {
-    tray?.setContextMenu(contextMenu);
   });
 }
 
