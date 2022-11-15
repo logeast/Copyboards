35aaba09e4be25c304db9738b909d631fc9649a9
logeast <logeast@outlook.com>
2022-11-15 09:50:19 +0800

    chore: update port

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import { copy } from "vite-electron-plugin/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 8848,
+  },
+  preview: {
+    port: 8848,
+  },
   build: {
     rollupOptions: {
       input: {
