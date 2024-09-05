832f7db88432ff5177d0009b48fb51579c16de12
logeast <logeast@outlook.com>
2024-09-06 00:12:26 +0800

    refactor: clipboard watcher

--- src-tauri/src/clipboard_watcher.rs
+++ src-tauri/src/clipboard_watcher.rs
@@ -5,6 +5,68 @@ use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use tauri::{AppHandle, Manager};
 
+fn should_update_content(
+    last_content: &Option<ClipboardContent>,
+    current_content: &ClipboardContent,
+) -> bool {
+    match (last_content, current_content) {
+        (Some(ClipboardContent::Image(last_image)), ClipboardContent::Image(current_image)) => {
+            last_image.hash != current_image.hash
+        }
+        (Some(ClipboardContent::Text(last_text)), ClipboardContent::Text(current_text)) => {
+            last_text != current_text
+        }
+        (Some(ClipboardContent::Color(last_color)), ClipboardContent::Color(current_color)) => {
+            last_color != current_color
+        }
+        _ => true,
+    }
+}
+
+fn process_image_content(content: &ClipboardContent, images_dir: &PathBuf) -> ClipboardContent {
+    if let ClipboardContent::Image(image) = content {
+        if let Some(data) = &image.data {
+            let arboard_image = arboard::ImageData {
+                width: image.width as usize,
+                height: image.height as usize,
+                bytes: std::borrow::Cow::Owned(data.clone()),
+            };
+            match utils::save_image(images_dir, &arboard_image) {
+                Ok(path) => {
+                    let mut new_image = image.clone();
+                    new_image.path = path;
+                    new_image.data = None;
+                    ClipboardContent::Image(new_image)
+                }
+                Err(e) => {
+                    eprintln!("Failed to save image: {}", e);
+                    ClipboardContent::Image(image.clone())
+                }
+            }
+        } else {
+            ClipboardContent::Image(image.clone())
+        }
+    } else {
+        content.clone()
+    }
+}
+
+fn update_clipboard_history(
+    clipboard_manager: &Arc<Mutex<ClipboardManager>>,
+    app_handle: &AppHandle,
+    content: ClipboardContent,
+) {
+    if let Err(e) = clipboard_manager
+        .lock()
+        .unwrap()
+        .add_item(content.clone(), None, None)
+    {
+        eprintln!("[✗]Failed to add item to clipboard history: {}", e);
+    } else {
+        app_handle.emit_all("clipboard-changed", &content).unwrap();
+    }
+}
+
 pub fn start_clipboard_watcher(
     clipboard_manager: Arc<Mutex<ClipboardManager>>,
     images_dir: PathBuf,
@@ -17,64 +79,12 @@ pub fn start_clipboard_watcher(
         loop {
             let current_content = utils::get_clipboard_content(&mut clipboard);
 
-            let should_update = match (&last_content, &current_content) {
-                (
-                    Some(ClipboardContent::Image(last_image)),
-                    ClipboardContent::Image(current_image),
-                ) => last_image.hash != current_image.hash,
-                (Some(ClipboardContent::Text(last_text)), ClipboardContent::Text(current_text)) => {
-                    last_text != current_text
-                }
-                (
-                    Some(ClipboardContent::Color(last_color)),
-                    ClipboardContent::Color(current_color),
-                ) => last_color != current_color,
-                _ => true,
-            };
-
-            if should_update {
-                let content_to_add = if let ClipboardContent::Image(image) = &current_content {
-                    if let Some(data) = &image.data {
-                        let arboard_image = arboard::ImageData {
-                            width: image.width as usize,
-                            height: image.height as usize,
-                            bytes: std::borrow::Cow::Owned(data.clone()),
-                        };
-                        // Save the image and update the path
-                        match utils::save_image(&images_dir, &arboard_image) {
-                            Ok(path) => {
-                                let mut new_image = image.clone();
-                                new_image.path = path;
-                                new_image.data = None;
-                                ClipboardContent::Image(new_image)
-                            }
-                            Err(e) => {
-                                eprintln!("Failed to save image: {}", e);
-                                current_content.clone()
-                            }
-                        }
-                    } else {
-                        current_content.clone()
-                    }
-                } else {
-                    current_content.clone()
-                };
-
-                if let Err(e) =
-                    clipboard_manager
-                        .lock()
-                        .unwrap()
-                        .add_item(content_to_add.clone(), None, None)
-                {
-                    eprintln!("[✗]Failed to add item to clipboard history: {}", e);
-                } else {
-                    app_handle
-                        .emit_all("clipboard-changed", &content_to_add)
-                        .unwrap();
-                }
-
+            if should_update_content(&last_content, &current_content) {
+                let content_to_add = process_image_content(&current_content, &images_dir);
+                update_clipboard_history(&clipboard_manager, &app_handle, content_to_add.clone());
                 last_content = Some(content_to_add);
             }
+
             std::thread::sleep(std::time::Duration::from_millis(500));
         }
     });
